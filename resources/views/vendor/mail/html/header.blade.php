9dd5c90afdaa060893b5becd4d1478d3c4e4e170
Manoj Kumar <manojkcoder@gmail.com>
2024-02-11 18:06:49 +0530

    Logo Size Fix

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
     <td class="header">
         <a href="{{ $url }}" style="display: inline-block;">
-            <img src="{{url('build/assets/logo-45a0b0dc.svg')}}" class="logo" alt="{{ config('app.name') }}">
+            <img src="{{url('build/assets/logo-45a0b0dc.svg')}}" class="logo" alt="{{ config('app.name') }}" height="250" width="80" style="height: 80px; width: 250px;">
         </a>
     </td>
 </tr>
